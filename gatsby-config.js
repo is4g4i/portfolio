8ff7be8bdec3ac3153e49c4a23dc64f19e6b65e8
Ricardo Gonzalez <ricardo.g@stateside.agency>
2022-07-24 13:02:27 -0600

    Finished Gatsby Tutorial Part 7

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,14 @@ module.exports = {
     "gatsby-plugin-sitemap",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-mdx",
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
       }
-    },
-    "gatsby-plugin-mdx",
+    }
   ]
 };
