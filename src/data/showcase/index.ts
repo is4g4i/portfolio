b114de1d0eec3cc8164544174dc2897cd309472a
root <root@SagaLaptop.localdomain>
2023-04-19 17:53:55 -0600

    Update anchors blank, added designs, new images

--- src/data/showcase/index.ts
+++ src/data/showcase/index.ts
@@ -1,18 +1,17 @@
-// adapted from https://github.com/withastro/astro.build/blob/112bdc723b3ba305997c95d7ce02304624d0d3ce/src/data/showcase/index.ts
-
 import type { Project } from "../../types";
 import sitesData from "./sites.json";
+import designsData from "./designs.json";
+
 
 const allImages = import.meta.glob<ImageMetadata>("./images/*.{png,jpg,jpeg}", {
   eager: true,
   import: "default",
 });
 
-let _loadShowcase: Promise<Array<Project>>;
-
-async function loadShowcase(): Promise<Array<Project>> {
+async function loadShowcase(type: string): Promise<Array<Project>> {
+  let data = type === 'projects' ? sitesData : designsData;
   const sites = await Promise.all(
-    sitesData.map(async (site) => {
+    data.map(async (site) => {
       if (!(site.image in allImages)) {
         console.error(
           `Image for "${site.title}" not found (provided: "${site.image}")`
@@ -31,7 +30,6 @@ async function loadShowcase(): Promise<Array<Project>> {
   return sites;
 }
 
-export async function getShowcase() {
-  _loadShowcase = _loadShowcase || loadShowcase();
-  return _loadShowcase;
+export async function getShowcase(type: 'projects' | 'designs') {
+  return loadShowcase(type);
 }
